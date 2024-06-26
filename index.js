2ff411f8adedf1dde1449aba30f3308ca0b1dd4d
Newton <newtonkirimi55@gmail.com>
2024-06-26 05:48:20 -0700

    JavaScript Strings

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage variable using template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage variable by converting welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define shortGreeting variable to include a shortened greeting with the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
